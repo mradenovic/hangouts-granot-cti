2a8d9e8635570b5964b9962c84012cd89fe5f7b0
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-09-17 19:15:29 -0400

    refactor(hangouts): reorganize event handling

--- app/scripts/hangouts/hangouts.js
+++ app/scripts/hangouts/hangouts.js
@@ -9,13 +9,14 @@ if (url.match(/StartPage#epreld/) || url.match(/gmail#epreld/)) {
   port.postMessage({hangoutsIsReady: true});
   port.onMessage.addListener(hangoutsOnMessage);
 
-  ready('span[dir="ltr"]', function(element){
-      var callerId = $(element).text();
-      console.log(callerId);
-      port.postMessage({callerId: callerId});
-  });
+  ready('span[dir="ltr"]', hangoutsOnInboundCall);
 }
 
 function hangoutsOnMessage(msg) {
   console.log(port.name, msg);
+}
+
+function hangoutsOnInboundCall(element) {
+  var callerId = $(element).text();
+  port.postMessage({callerId: callerId});
 }
