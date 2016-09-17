2852e916be1cd6c7be3deea46970c954dedea4ad
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-09-17 11:27:41 -0400

    refactor(hangouts): remove redundant code

--- app/scripts/hangouts/hangouts.js
+++ app/scripts/hangouts/hangouts.js
@@ -4,10 +4,8 @@ var port;
 var url = document.URL;
 
 if (url.match(/StartPage#epreld/)) {
-  console.log(url);
   port = chrome.runtime.connect({name: 'hangouts'});
 
-  port.postMessage({text: 'Hello, Background'});
   port.onMessage.addListener(hangoutsPortListener);
 
   ready('span[dir="ltr"]', function(element){
