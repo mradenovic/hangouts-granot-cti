0f79d51de9e765d404de163997f2b75fff4f3046
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-09-17 10:55:34 -0400

    fix(hangouts): execute script only within hangouts

--- app/scripts/hangouts/hangouts.js
+++ app/scripts/hangouts/hangouts.js
@@ -1,13 +1,20 @@
 'use strict';
 
-var port = chrome.runtime.connect({name: 'hangouts'});
-port.postMessage({text: 'Hello, Background'});
-port.onMessage.addListener(hangoutsPortListener);
+var port;
+var url = document.URL;
+
+if (url.match(/StartPage#epreld/)) {
+  console.log(url);
+  port = chrome.runtime.connect({name: 'hangouts'});
+
+  port.postMessage({text: 'Hello, Background'});
+  port.onMessage.addListener(hangoutsPortListener);
+
+  ready('span[dir="ltr"]', function(element){
+      console.log($(element).text());
+  });
+}
 
 function hangoutsPortListener(msg) {
   console.log(port.name, msg);
 }
-
-ready('span[dir="ltr"]', function(element){
-    console.log($(element).text());
-});
